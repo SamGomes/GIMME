f1936f2396893eb3db8d80e38822986af462f5f7
Samuel Gomes <sam.s.gomes@sapo.pt>
2023-11-03 11:22:42 +0000

    Updated optional script to run simulations in Windows PowerShell.

--- examples/simulations/runGIMMESims.bat
+++
@@ -1,5 +0,0 @@
-C:
-G:
-cd G:\GIMME\examples\simulations
-cls
-python simulationsEvl.py
---
+++ examples/simulations/runGIMMESims.ps1
@@ -0,0 +1,12 @@
+
+$NUM_PROCESSES = 2
+for ($i=0; $i -lt $NUM_PROCESSES; $i++)
+{
+    invoke-expression 'wt -w 0 powershell -Command{
+        $myshell = New-Object -ComObject wscript.shell;
+        cd C:\Users\samsg\OneDrive\Documents\reps_aux\GIMME;
+        clear;
+        ..\venv\Scripts\Activate.ps1;
+        python .\examples\simulations\simulations.py
+    }'
+}
